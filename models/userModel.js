96f359d51a6fee0a5dd2f5e77e4a921c16be2a5a
Hady Maher <Hady207@hotmail.com>
2019-10-19 15:13:36 +0300

    changed the name of the project

--- models/userModel.js
+++ models/userModel.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
       validator: function(el) {
         return el === this.password;
       },
-      message: `Password are not the same`
+      message: `Passwords are not the same`
     }
   },
   passwordChangedAt: Date,
