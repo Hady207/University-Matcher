bb8651972bac86e5ef1c35f58bf42b9a36c84eec
Hady Maher <Hady207@hotmail.com>
2019-11-13 20:17:01 +0300

    Secure HTTPS Connections

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const viewRoute = require('./routes/viewRoute');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
