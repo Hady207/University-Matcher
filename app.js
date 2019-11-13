aa73559d90769dfe80db27783954a799e391e97e
Hady Maher <Hady207@hotmail.com>
2019-11-13 03:40:18 +0300

    security packages added

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+// external installed modules
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const compression = require('compression');
 
+// local modules
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const universityRoute = require('./routes/universityRoute');
@@ -18,13 +25,50 @@ const app = express();
 // Serving static files
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Modules Middleware
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
 app.use(cors());
 
 app.use((req, res, next) => {
