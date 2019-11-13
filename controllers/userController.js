1cea0d78fcf0cf13f4facd2b001e585d1b60ca43
Hady Maher <Hady207@hotmail.com>
2019-11-13 03:48:10 +0300

    removed conosle.logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,8 +45,8 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
+  // console.log(req.file);
+  // console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return new AppError(
@@ -74,7 +74,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 exports.follow = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.user.id);
   // const friend = await User.findById(req.params.id);
-  console.log('me', user);
+  // console.log('me', user);
   // console.log('you', friend);
   if (!user) {
     return next(new AppError('no user with given id', 400));
